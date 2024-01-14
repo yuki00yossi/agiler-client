0e8656d9ce0f99ebd09b633a132a73e4d741db90
yuki00yossi <yuki00yoshioka@gmail.com>
2024-01-14 21:14:19 +0900

    基本デザイン修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
 				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
 					<div className="relative flex flex-col h-screen w-screen overflow-x-hidden">
 						<Navbar />
-						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow max-w-[1280px]">
 							{children}
 						</main>
 						<footer className="w-full flex items-center justify-center py-3">
