c7ae96933070186ef2526d448fc5c71e96f369bc
yuki00yossi <yuki00yoshioka@gmail.com>
2024-01-14 23:02:30 +0900

    ビルドエラー対応

--- utils/validators.tsx
+++ utils/validators.tsx
@@ -2,9 +2,3 @@ export const emailValidator = (email: string) => {
     const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
     return expression.test(email);
 }
-
-export const passwordValidator = (email: string) => {
-    // const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-    // const expression: RegExp =  /^$/i/;
-    return expression.test(email);
-}
